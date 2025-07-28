ff32ea5f6ec2e77a0570abce4c7a608c70b80a15
Reon <minherwub@gmail.com>
2025-07-28 12:13:22 +0800

    Update hw0a

--- hw0a/tests/JavaExercisesTest.java
+++ hw0a/tests/JavaExercisesTest.java
@@ -11,10 +11,40 @@ public class JavaExercisesTest {
         // Visually check the console output!
     }
 
+    public class StarTriangle {
+        public static void main(String[] args) {
+            int row = 5;
+            for (int i = 0; i < row; i ++) {
+
+                for (int j = 0; j < row - i -1; j ++) {
+                    System.out.print(" ");
+                }
+                for (int k = 0; k <= i; k ++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+
     @Test
     public void testPrintIndexed() {
         JavaExercises.printIndexed("hello");
         // Visually check the console output!
+    }
+
+    public class PrintIndexed {
+        public static void PrintIndexed(String s) {
+            String output = "";
+            for (int i = 0; i < s.length(); i ++) {
+                output += s.charAt(i);
+                output += String.valueOf(s.length() - 1 - i);
+            }
+            System.out.println(output);
+        }
+        public static void main(String[] args) {
+            PrintIndexed ("hello");
+        }
     }
 
     @Test
@@ -24,7 +54,21 @@ public class JavaExercisesTest {
         assertThat(JavaExercises.stutter("")).isEqualTo("");
     }
 
-    @Test
+    public class stutter {
+        public static void Stutter(String s) {
+            String output = "";
+            for (int i = 0; i < s.length(); i ++) {
+                output += s.charAt(i);
+                output += s.charAt(i);
+            }
+            System.out.println(output);
+        }
+        public static void main(String[] args) {
+            Stutter ("hello");
+        }
+
+
+        @Test
     public void testQuadrant() {
         assertThat(JavaExercises.quadrant(3, 4)).isEqualTo(1);
         assertThat(JavaExercises.quadrant(-3, 4)).isEqualTo(2);
@@ -34,3 +78,26 @@ public class JavaExercisesTest {
         assertThat(JavaExercises.quadrant(5, 0)).isEqualTo(0);
     }
 }
+
+    public class Quadrant {
+        public static String getQuadrant(int x, int y) {
+            String output = "";
+
+            if (x == 0 || y == 0) {
+                return "0";
+            } else if (x > 0 && y > 0) {
+                return "1";
+            } else if (x < 0 && y > 0) {
+                return "2";
+            } else if (x < 0 && y < 0) {
+                return "3";
+            } else {
+                return "4";
+            }
+        }
+
+        public static void main(String[] args) {
+            String result = getQuadrant(-2, 6);
+            System.out.println("Quadrant:" + result);
+        }
+    }
